bcc50e0a84def3263ff802be11b3f2e3cf50fd29
filipboshevski <filipbosevski1234@gmail.com>
2020-07-30 17:19:45 +0200

    Updates

---
+++ precache-manifest.2700686bc8fbf65c8656eefdb3594843.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b156f1f73c994881dd1efa1312283fb4",
+    "url": "/smart-face-by-filip/index.html"
+  },
+  {
+    "revision": "08bae871eb88416dd323",
+    "url": "/smart-face-by-filip/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "4fec096ebad3df7a9aeb",
+    "url": "/smart-face-by-filip/static/css/main.85f5fd4f.chunk.css"
+  },
+  {
+    "revision": "08bae871eb88416dd323",
+    "url": "/smart-face-by-filip/static/js/2.8612a597.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/smart-face-by-filip/static/js/2.8612a597.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fec096ebad3df7a9aeb",
+    "url": "/smart-face-by-filip/static/js/main.2c7e2b63.chunk.js"
+  },
+  {
+    "revision": "10dffe5221697d7ea45f",
+    "url": "/smart-face-by-filip/static/js/runtime-main.51478622.js"
+  },
+  {
+    "revision": "c555da565e9235e2e28ea78dd9926593",
+    "url": "/smart-face-by-filip/static/media/All The Roll - Personal Use.c555da56.otf"
+  },
+  {
+    "revision": "0dd2024bc0d74470849532878643b74e",
+    "url": "/smart-face-by-filip/static/media/brain.0dd2024b.png"
+  }
+]);
